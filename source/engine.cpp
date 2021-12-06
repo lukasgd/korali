b7cae2a63f4fdecdeaf103891d076ae61eac8683
Sergio Martin <martiser@ethz.ch>
2021-12-06 15:16:11 +0100

    Updating engine configuration

--- source/engine.cpp
+++ source/engine.cpp
@@ -30,7 +30,19 @@ Engine::Engine()
   gsl_set_error_handler_off();
 }
 
-void Engine::initialize()
+void Engine::initializeExperiments()
+{
+  // Initializing experiment's configuration
+  for (size_t i = 0; i < _experimentVector.size(); i++)
+  {
+    _experimentVector[i]->_experimentId = i;
+    _experimentVector[i]->_engine = this;
+    _experimentVector[i]->initialize();
+    _experimentVector[i]->_isFinished = false;
+  }
+}
+
+void Engine::start()
 {
   // Setting Engine configuration defaults
   if (!isDefined(_js.getJson(), "Profiling", "Detail")) _js["Profiling"]["Detail"] = "None";
@@ -45,29 +57,6 @@ void Engine::initialize()
   _profilingDetail = _js["Profiling"]["Detail"];
   _profilingFrequency = _js["Profiling"]["Frequency"];
 
-  // Initializing experiment's configuration
-  for (size_t i = 0; i < _experimentVector.size(); i++)
-  {
-    _experimentVector[i]->_experimentId = i;
-    _experimentVector[i]->_engine = this;
-    _experimentVector[i]->initialize();
-    _experimentVector[i]->_isFinished = false;
-  }
-
-  // Check configuration correctness
-  auto js = _js.getJson();
-  if (isDefined(js, "Conduit")) eraseValue(js, "Conduit");
-  if (isDefined(js, "Dry Run")) eraseValue(js, "Dry Run");
-  if (isDefined(js, "Conduit", "Type")) eraseValue(js, "Conduit", "Type");
-  if (isDefined(js, "Profiling", "Detail")) eraseValue(js, "Profiling", "Detail");
-  if (isDefined(js, "Profiling", "Path")) eraseValue(js, "Profiling", "Path");
-  if (isDefined(js, "Profiling", "Frequency")) eraseValue(js, "Profiling", "Frequency");
-
-  if (isEmpty(js) == false) KORALI_LOG_ERROR("Unrecognized settings for Korali's Engine: \n%s\n", js.dump(2).c_str());
-}
-
-void Engine::start()
-{
   // Checking if its a dry run and return if it is
   if (_isDryRun) return;
 
@@ -86,8 +75,19 @@ void Engine::start()
   // Recovering Conduit configuration in case of restart
   _conduit->getConfiguration(_js.getJson()["Conduit"]);
 
-  // Now initializing engine and its experiments
-  initialize();
+  // Now initializing experiments
+  initializeExperiments();
+
+  // Check configuration correctness
+  auto js = _js.getJson();
+  if (isDefined(js, "Conduit")) eraseValue(js, "Conduit");
+  if (isDefined(js, "Dry Run")) eraseValue(js, "Dry Run");
+  if (isDefined(js, "Conduit", "Type")) eraseValue(js, "Conduit", "Type");
+  if (isDefined(js, "Profiling", "Detail")) eraseValue(js, "Profiling", "Detail");
+  if (isDefined(js, "Profiling", "Path")) eraseValue(js, "Profiling", "Path");
+  if (isDefined(js, "Profiling", "Frequency")) eraseValue(js, "Profiling", "Frequency");
+
+  if (isEmpty(js) == false) KORALI_LOG_ERROR("Unrecognized settings for Korali's Engine: \n%s\n", js.dump(2).c_str());
 
   if (_conduit->isRoot())
   {
